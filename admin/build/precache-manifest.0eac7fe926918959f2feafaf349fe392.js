51fb9c03f30ec92bdf472212234f9a40d4617f4f
olegkatuba <olegkatuba@gmail.com>
2018-11-22 20:51:14 +0300

    add admin build

---
+++ admin/build/precache-manifest.0eac7fe926918959f2feafaf349fe392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "191aa0b2f8a6b939dea4",
+    "url": "/static/js/main.191aa0b2.chunk.js"
+  },
+  {
+    "revision": "e5f1491139e085ca120f",
+    "url": "/static/js/1.e5f14911.chunk.js"
+  },
+  {
+    "revision": "191aa0b2f8a6b939dea4",
+    "url": "/static/css/main.a8774dda.chunk.css"
+  },
+  {
+    "revision": "7256a97aeeaae851317bf3e6ed01a74c",
+    "url": "/index.html"
+  }
+];
